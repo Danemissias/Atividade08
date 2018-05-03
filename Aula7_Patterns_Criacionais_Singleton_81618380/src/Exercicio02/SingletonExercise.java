99a2cd1fa1c1843342218000efc47d2b9e4d1723
Alunos <Alunos@MOCLI10121.animaedu.intranet>
2018-05-02 21:41:03 -0300

    Atividade08

---
+++ Aula7_Patterns_Criacionais_Singleton_81618380/src/Exercicio02/SingletonExercise.java
@@ -0,0 +1,12 @@
+package Exercicio02;
+
+//* Daniel Missias dos Santos - 81618380
+
+public class SingletonExercise {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
